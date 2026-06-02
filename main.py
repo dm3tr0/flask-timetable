5b17cfbe35930d2e7c28bb30de7d0d6631607212
Dmytro <dima.ivanic@gmail.com>
2026-06-02 19:33:42 +0300

    Added context

--- main.py
+++ main.py
@@ -6,9 +6,21 @@ app = Flask(__name__)
 def main():
     return render_template("base.html")
 
+@app.route("/timetable")
 def timetable():
-    return render_template("timetable.html")
-app.add_url_rule("/timetable", "timetable_path", timetable)
+    city = "Odessa"
+    timetable = [
+        ["Bus 2", "18:00"],
+        ["Train 17", "15:00"],
+        ["Tram 33", "14:30"],
+        ["Plane 111", "2:30"],
+    ]
+    context = {
+        'timetable': timetable,
+        'city': city,
+        'role': "User",
+    }
+    return render_template("timetable.html", **context)
 
 
 @app.route("/user/<name>")
